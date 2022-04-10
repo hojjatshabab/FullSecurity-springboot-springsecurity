3062a2ffbaf83e50d8de66d34da118276c76ff86
hojjatshabab <hojatsba@gmail.com>
2022-04-10 12:59:00 +0430

    Add cors to securityConfig

--- src/main/java/ir/it/fullsecurity/security/SecurityConfig.java
+++ src/main/java/ir/it/fullsecurity/security/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         /*CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
         customAuthenticationFilter.setFilterProcessesUrl("/api/login");*/
+        http.cors().and();
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
         //http.authorizeRequests().antMatchers("/swagger-ui.html","swagger-ui/index.html").permitAll();
